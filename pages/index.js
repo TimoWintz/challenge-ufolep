810111061ac893102f25890a71a1d623644b25cd
TimoWintz <timothee@timwin.fr>
2024-01-28 14:42:15 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,15 +6,12 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Challenge Ufolep 38</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+         <p>Bientôt le challenge Ufolep 38 cyclosport sur cette page.</p>
       </main>
 
       <Footer />
