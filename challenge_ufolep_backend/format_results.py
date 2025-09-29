09a922457d46bfd93f4b0ce699f2d1677307a199
Tim <timothee@timwin.fr>
2025-09-29 08:26:14 +0200

    murianette

--- challenge_ufolep_backend/format_results.py
+++ challenge_ufolep_backend/format_results.py
@@ -108,7 +108,7 @@ class GenericCSVFormatter(ResultsFormatter):
     COLS_NAME = ["NOM", "Nom", "Nom complet"]
     COLS_SURNAME = ["Prénom", "Prenom"]
     COLS_PLACE = ["Place", "Rang", "Arrivée", "Clst", "Class.","Classement"]
-    COLS_CAT = ["Caté. UFOLEP", "Catégorie", "Catégorie Age"]
+    COLS_CAT = ["Caté. UFOLEP", "Catégorie", "Catégorie Age", "Fédération / catégorie"]
     COLS_CLUB = ["Club", "CLUB", "Club Ufolep 38"]
 
     VALUES_DNF = ["Abandon", "Ab", "AB", "DNF"]
